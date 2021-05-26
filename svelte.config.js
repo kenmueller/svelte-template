d43b8de8f604757fd46149abf88b9992ac33adc9
kenmueller <kenmueller0@gmail.com>
2021-05-26 14:00:57 -0700

    immutable

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import preprocess from 'svelte-preprocess'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
+	compilerOptions: {
+		immutable: true
+	},
 	kit: {
 		target: '#root',
 		files: {
